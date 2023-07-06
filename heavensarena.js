74c19d85671856fc21752a941c4dca8cc614b7d3
matiasecharri <matiasecharri@hotmail.com>
2023-07-06 00:54:01 -0300

    2/5?

--- heavensarena.js
+++ heavensarena.js
@@ -1,20 +1,31 @@
 //HA1
-// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
-// 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 
-const wordCounter = (word) => {
+// 1) Programa una función que cuente el número de caracteres de una cadena de texto.
+const stringCounter = (word) => {
   if (typeof word === "string") {
-    return `${word} tiene ${word.length} caracteres.`
-  } else if(typeof word === "number"){
-      let numberToString = word.toString()
-      return `${numberToString} tiene ${numberToString.length} caracteres y ahora es un string xD.`     
-   } else {
-    return `${word} deberia ser una cadena de texto.`
+    return `${word} tiene ${word.length} caracteres.`;
+  } else if (typeof word === "number" || typeof word === "object") {
+    let numberToString = word.toString();
+    return `${numberToString} tiene ${numberToString.length} caracteres y ahora es un string xD.`;
+  } else {
+    return `${word} deberia ser una cadena de texto.`;
   }
 };
+console.log(stringCounter("hola2"));
+console.log(stringCounter(["perasdasdasds"]));
 
-console.log(wordCounter("hola2"))
-console.log(wordCounter(789789789))
+// 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 
+const wordBlade = (word, corte1, corte2) => {
+  let cuttedWord = word.toString().slice(corte1, corte2);
+  if (typeof cuttedWord === "string") {
+    return cuttedWord;
+  } else {console.log("No puedo cortar eso")}
+};
+
+console.log(wordBlade("Oso Enojado", 0, 4));
+console.log(wordBlade(2312312321, 0, 4));
+console.log(wordBlade(["Piedra gigante", "Interior de la piedra", "Oro", "Veneno"], 37, 40));
