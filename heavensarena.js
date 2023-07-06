cebc20c124c245c644a738894fe04dc5c21939b5
matiasecharri <matiasecharri@hotmail.com>
2023-07-06 19:55:35 -0300

    finish HA1

--- heavensarena.js
+++ heavensarena.js
@@ -1,25 +1,27 @@
 //HA1
 
-// 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
-
-// 1) Programa una función que cuente el número de caracteres de una cadena de texto.
-
+// 1)
+console.warn(
+  "👁️ Programa una función que cuente el número de caracteres de una cadena de texto."
+);
 const stringCounter = (item) => {
   if (typeof item === "string") {
-    console.log(`${item} tiene ${item.length} caracteres.`);
+    console.log(`${item} tiene "${item.length}" caracteres.`);
   } else if (typeof item === "number" || typeof item === "boolean") {
     let xToString = item.toString();
     console.log(
-      `${xToString} tiene ${xToString.length} caracteres y es ahora un string xD;`
+      `${xToString}: tiene "${xToString.length}" caracteres era un numero y es ahora un string.`
     );
   } else if (typeof item === "object") {
     const keySearcher = Object.keys(item);
-    console.log(keySearcher.length);
+    console.log(
+      `Esto era un objeto con "${keySearcher.length}" propiedades.`
+    );
   } else {
     console.warn("No ingresaste nada papito o es un tipo de dato no válido.");
   }
 };
-stringCounter("hola2");
+stringCounter("Cuenta estos caracteres");
 stringCounter(31231312);
 stringCounter({
   nombre: "Manolito",
@@ -28,7 +30,10 @@ stringCounter({
   almuerzo: ["Gambas", "Peras", "Maranjas"],
 });
 
-// 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+console.warn(
+  "👁️ Programa una función que te devuelva el texto recortado según el número de caracteres indicados"
+);
+// 2
 
 const wordBlade = (word, corte1, corte2) => {
   let cuttedWord = word.toString().slice(corte1, corte2);
@@ -41,13 +46,36 @@ const wordBlade = (word, corte1, corte2) => {
 
 console.log(wordBlade("Oso Feliz Atacandote", 0, 4));
 console.log(wordBlade(213123123213213123, 0, 1));
-console.log(
-  wordBlade(
-    ["Piedra gigante", "Interior de la piedra", "Diamante", "Veneno"],
-    37,
-    45
-  )
+console.log(wordBlade(["Piedra gigante", "Interior de la piedra", "Diamante", "Veneno"], 37,45));
+
+// 3)
+console.warn(
+  "👁️ Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter"
 );
 
-// 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+const pato = "Hola como estasss gorda";
 
+const wordSeparator = (word, separator) => {
+  if (typeof word === "string") {
+    let wordToArray = word.split(separator);
+    console.log(wordToArray);
+  } else {
+    console.warn("Se esperaba una cadena de texto.");
+  }
+};
+wordSeparator("Ready for tomorrow", "");
+
+// 4)
+console.warn(
+  "👁️Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo."
+);
+
+const wordRepeater = (word, repeat) => {
+  if (typeof repeat === "number" && typeof word === "string") {
+    for (let i = 0; i < repeat; i++) {
+      console.log(`Tu palabra ${word} se repitio ${i} vez/veces.`);
+    }
+  } else {console.clear() + console.warn(`Debes ingresar una palabra y un número, en ese orden.`)}
+};
+
+wordRepeater("xD", 10);
