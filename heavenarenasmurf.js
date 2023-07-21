716e6201738af76da582580199cb9261a55bd409
matiasecharri <matiasecharri@hotmail.com>
2023-07-21 18:39:51 -0300

    1 to 10

---
+++ heavenarenasmurf.js
@@ -0,0 +1,45 @@
+console.clear();
+console.log("HEAVEN ARENA SMURF 🤖"); 
+
+
+
+
+// console.warn(
+//   "👁️ Programa una función que cuente el número de caracteres de una cadena de texto."
+// );
+
+// console.warn(
+//     "👁️ Programa una función que te devuelva el texto recortado según el número de caracteres indicados"
+//   );
+
+// console.warn(
+//     "👁️ Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter"
+//   );
+
+// console.warn(
+//     "👁️Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo."
+//   );
+
+// console.warn(
+//     "👁️ Programa una función que invierta las palabras de una cadena de texto, pe"
+//   );
+
+// console.warn(
+//     "👁️ Programa una función para contar el número de veces que se repite una palabra en un texto largo"
+//   );
+
+// console.warn(
+//     "👁️ Programa una función que valide si una palabra o frase dada, es un palíndromo"
+//   );
+
+// console.warn(
+//     `👁️ Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5`
+//   );
+
+// console.warn(
+//     `👁️ Programa una función que obtenga un numero aleatorio entre 501 y 600.`
+//   );
+
+// console.warn(
+//     `👁️ Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true`
+//   );
