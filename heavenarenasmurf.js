d16beef8c46f59a822f9b7c9cfa13851b010e020
matiasecharri <matiasecharri@hotmail.com>
2023-07-29 17:09:24 -0300

    up

--- heavenarenasmurf.js
+++ heavenarenasmurf.js
@@ -511,7 +511,7 @@ console.warn(
 
 const higherAndLower = (array = "") => {
   if (array === "") {
-    console.warn("Array and Pow cant be empty.");
+    console.warn("Array cant be empty.");
     return;
   }
   if (!(array instanceof Array)) {
@@ -529,14 +529,18 @@ const higherAndLower = (array = "") => {
   console.log(higherNumber);
   console.log(lowerNumber);
 };
-higherAndLower(numberElevator(numberArray, 2));
+higherAndLower(numberArray, 2);
 //----------------------------------------------------------------------------------------------------------
 
 console.warn(
   `👁️Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.`
 );
-const array2 = [2, 4, 13, 15];
-const evenAndOdd = (array) => {
+const array2 = [200, 2, 4, 13, 15];
+const evenAndOdd = (array = "") => {
+  if (array === "") {
+    console.warn("Array cant be empty.");
+    return;
+  }
   if (!(array instanceof Array)) {
     console.error("Needs to be an array.");
     return;
@@ -567,6 +571,35 @@ console.log(evenAndOdd(numberElevator(numberArray, 5)).odds);
 console.warn(
   `👁️Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5]`
 );
+
+const arrayOrdenator = (array) => {
+  if (array === "") {
+    console.warn("Array cant be empty.");
+    return;
+  }
+  if (!(array instanceof Array)) {
+    console.error("Needs to be an array.");
+    return;
+  }
+  array.some((x) => {
+    if (typeof x !== "number") {
+      console.error("Some elements inside the array are not numbers.");
+      return;
+    }
+  });
+  const arrayAsc = [...array];
+  const arrayDesc = [...array];
+
+  arrayAsc.sort((a, b) => a - b);
+  arrayDesc.sort((a, b) => b - a);
+
+  return {
+    arrayAsc,
+    arrayDesc,
+  };
+};
+console.log(arrayOrdenator(array2).arrayAsc);
+console.log(arrayOrdenator(array2).arrayDesc);
 
 console.warn(
   `👁️Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].`
