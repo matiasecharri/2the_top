f12c31eac7c0046118ce3f10df8ae049771abb78
matiasecharri <matiasecharri@hotmail.com>
2023-08-10 12:10:33 -0300

    up

--- TheoryAndPractice/script4.js
+++ TheoryAndPractice/script4.js
@@ -150,27 +150,42 @@ const arrDeLoIterado = [...iterableF()]; //Guardamos cada yield en un array.
 console.log(arrDeLoIterado);
 
 // ✅PROXIES ----------------------------------------------------//
-const PERSONA = {
-    nombre: "",
-    edad: 0,
-    apellido: "", //Objeto MODELO
-  };
+// const PERSONA = {
+//     nombre: "",
+//     edad: 0,
+//     apellido: "", //Objeto MODELO
+//   };
   
-  const MANEJADOR = {
-    set(obj, prop, value) {
-      if (Object.keys(obj).includes(prop)) { //Manejo de validaciones 
-        obj[prop] = value;
-      } else {
-        console.error(`La propiedad ${prop} no existe.`);
-      }
-    },
-  };
+//   const MANEJADOR = {
+//     set(obj, prop, value) {
+//       if (Object.keys(obj).includes(prop)) { //Manejo de validaciones 
+//         obj[prop] = value;
+//       } else {
+//         console.error(`La propiedad ${prop} no existe.`);
+//       }
+//     },
+//   };
   
-  const DIANA = new Proxy(PERSONA, MANEJADOR); //Nueva instancia, muy parecido
-  DIANA.nombre = "Diana";
-  DIANA.edad = 23;
-  DIANA.apellido = "Vodo";
-  DIANA.twitter = "@DIANAV";
+//   const DIANA = new Proxy(PERSONA, MANEJADOR); //Nueva instancia, muy parecido
+//   DIANA.nombre = "Diana";
+//   DIANA.edad = 23;
+//   DIANA.apellido = "Vodo";
   
-  console.log(DIANA);
+//   console.log(DIANA);
+
+  // ✅COMPUTE PROPERTIES IN OBJECTS -----------------------------------//
+
+let aleatorio = Math.round(Math.random() * 100 + 5)
+const OBJ_USUARIOS = {
+    propiedad: "value", //Propiedad clasica
+    [`id_${aleatorio}`]:"random value" //Propiedad dinamica
+  }  
+console.log(OBJ_USUARIOS)
+
+  const ARRAY_USUARIOS = ["Michael", "Dwight", "Jim", "Pam"]
+  ARRAY_USUARIOS.forEach((x, i)=>OBJ_USUARIOS[`id_${i}`] = x)
+  //Los corchetes indican que una propiedad se define
+  //de forma dinámica.
+  console.log(OBJ_USUARIOS)
+
   
