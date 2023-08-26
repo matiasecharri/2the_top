ae3ea8f635a99cd1113feecd352f7a9de1989278
matiasecharri <matiasecharri@hotmail.com>
2023-08-26 19:02:34 -0300

    template+fragment+export

---
+++ Dom/04_DOM/array.js
@@ -0,0 +1,82 @@
+const messages = [
+  {
+    id: 1,
+    message: ` もしも未来のサイバーパンク世界でテキストメッセージを受け取るとしたら、それは確かに驚きと興奮に満ちた瞬間でしょう。機械と人間が交差する未来の光景の中で、デジタルなネオンの光が点滅する中、スマートデバイスが瞬時に手の中に情報を届けることは、何か魔法のような感覚をもたらすかもしれません。<br /><br />
+  
+      ハッカーの魔法のような指先が、キーボードを舞い踊り、デジタルな暗号が送られ、ネットワークを通じてメッセージが流れる。スクリーンに浮かび上がる文字は、古典的な手紙のような感情を伝える一方で、未来のテクノロジーの魔法によって生まれたものでもあります。<br /><br />
+  
+      サイバーパンクの世界では、情報の交換が過去のような方法とは異なり、デジタルな手段によって行われるでしょう。しかし、テキストメッセージを通じて伝わる感情や思いは、どの時代においても価値あるものです。未来の都市の喧騒の中で、一瞬だけ時間が止まり、誰かからのメッセージを受け取る瞬間は、人間性とテクノロジーの絶妙な融合を感じる瞬間となるでしょう。
+      `,
+  },
+  {
+    id: 2,
+    message: `
+      Getting a text in this world, where dermal implants turn you into a
+      bulletproof wonder, now that's a story. In the midst of these neon
+      lights, where man and machine tango like no tomorrow, and bam! Your
+      trusty gadget serves you that message, easy as pie. It's almost like
+      magic, a real cyber spell, believe me.<br /><br />
+  
+      Fingers tapping on keys, codes flowing through the veins of the web,
+      messages cruising in the digital breeze. Those words popping up on
+      your screen, they're like old-school letters but with this
+      futuristic techno spin, you know what I mean?<br /><br />
+  
+      In this cyberpunk scene, sharing info ain't like the old days, no
+      sir. It's all about the digital game. But the emotions, the
+      thoughts, they're still as real as they come. Amid the chaos of the
+      city, just for a heartbeat, time sort of slows when that message
+      drops. It's a moment where you and the tech, the body and the
+      machine, they all sync up like a well-oiled dream, if you catch my
+      drift.`,
+  },
+  {
+    id: 3,
+    message: `
+      As credits flow through the digital veins of commerce, and neon-lit streets are alive with holographic advertisements, receiving a text feels like a personal whisper in a crowded megacity. Fingers deftly dance on virtual interfaces, encrypted messages navigate the labyrinthine network, and in an instant, your reliable device projects the message into your augmented reality. It's like a digital secret passed through invisible hands, a testament to the new economy we thrive in.<br /><br />
+  
+      Words materialize before your eyes, a fusion of classic communication and the wonders of neural-link technology. Comprends-tu?<br /><br />
+  
+      Amidst the urban symphony of progress and innovation, sharing sentiments and thoughts has morphed, yet their essence remains timeless. Amidst the chaos of corporations and neon, time momentarily pauses as the message arrives—a fleeting connection between your soul and the pulse of futuristic life.
+      `,
+  },
+
+  ,
+  {
+    id: 4,
+    message: `
+      Dans un futur où les crédits cybernetic implantscybernetic implants numériques dictent les mouvements de l'économie et où les senteurs des stands de rue se mêlent à la lueur des néons, recevoir un message texte devient un trésor précieux. Les doigts glissent sur des interfaces cybernétiques, les messages chiffrés empruntent les voies neuronales, et soudainement, votre appareil fiable projette le message dans votre champ visuel. C'est comme une lettre d'amour futuriste, un symbole de connexion dans une ère d'abondance technologique.<br /><br />
+  
+      Les mots se matérialisent dans votre interface augmentée, une fusion élégante entre communication classique et merveilles de l'innovation cybernétique. Comprenez-vous?<br /><br />
+  
+      Au milieu de la toile sensorielle de la vie urbaine et des délices culinaires, l'échange d'émotions et d'idées prend une nouvelle forme, tout en préservant leur authenticité. Au milieu du vacarme des annonces lumineuses et des merveilles biomécaniques, le temps semble momentanément s'arrêter lorsque le message arrive, une synchronisation éphémère entre votre essence et la mosaïque du futur.
+      `,
+  },
+  {
+    id: 5,
+    message: `
+    デジタルクレジットが経済を牽引し、ネオンの輝きが街を埋め尽くす世界。そこでテキストメッセージを受け取る瞬間は、賑やかな都市の中での個人的なささやきのよう。指先が仮想キーボードを踊り、暗号化されたメッセージがネットワークを駆け巡り、あっという間に、信頼のおけるデバイスがメッセージをオーグメントされた現実の中に投影します。それはまるで目に見えない手を通して運ばれるデジタルな秘密のようであり、私たちが生きる新しい経済の象徴です。<br /><br />
+  
+    言葉は視界に浮かび上がり、古典的なコミュニケーションと神経リンク技術の驚異的な融合。分かりますか？<br /><br />
+  
+    進歩と革新の都市のシンフォニーの中で、感情や思考を共有するアートは変化しましたが、その本質は不変です。法人とネオンの混沌の中で、メッセージが届くと時間は一瞬止まるような、あなたの魂と未来の鼓動とのつながりを感じる瞬間です。
+    `,
+  },
+  {
+    id: 6,
+    message: `In the heart of this advanced era, where neon lights paint the skyline and the symphony of technology and humanity intertwines, there exists a hidden gem, an oasis of flavor amidst the torrent of information. This place, a small haven of tradition in a sea of innovation, is renowned for serving some of the finest ramen in the city. Its unassuming facade and warm ambiance stand in contrast to the cybernetic marvels that surround us.<br /><br />
+  
+    Visitors, many of them enhanced individuals with cybernetic implants, flock here in search of an experience that goes beyond the buzz of networks and data. In the dimness illuminated by subtle holographic lights, they immerse themselves in the tantalizing aroma of ramen, flavors that evoke bygone times while harmonizing with the futuristic aesthetics. It's a reminder that, amidst all the innovation, the roots of culture still find their place in the heart of this exhilarating era.
+    `,
+  },
+  {
+    id: 7,
+    message: `
+    In the heart of this advanced era, where neon lights paint the skyline and the symphony of technology and humanity intertwines, there exists a hidden gem, an oasis of flavor amidst the torrent of information. This place, a small haven of tradition in a sea of innovation, is renowned for serving some of the finest ramen in the city. Its unassuming facade and warm ambiance stand in contrast to the cybernetic marvels that surround us.<br /><br />
+  
+    Visitors, many of them enhanced individuals with cybernetic implants, flock here in search of an experience that goes beyond the buzz of networks and data. In the dimness illuminated by subtle holographic lights, they immerse themselves in the tantalizing aroma of ramen, flavors that evoke bygone times while harmonizing with the futuristic aesthetics. It's a reminder that, amidst all the innovation, the roots of culture still find their place in the heart of this exhilarating era.
+    `,
+  },
+];
+
+export { messages };
