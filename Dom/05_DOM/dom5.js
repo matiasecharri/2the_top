15376a0bdccb646630b9b0f632cdaa22bb13ec58
matiasecharri <matiasecharri@hotmail.com>
2023-08-28 02:40:44 -0300

    up

--- Dom/05_DOM/dom5.js
+++ Dom/05_DOM/dom5.js
@@ -1,1 +1,15 @@
-// ⚡69. DOM 8 - Templates HTML
+// ⚡72. DOM 12 Events
+const $button = document.getElementById("button1")
+$button.addEventListener("click", (e)=>{
+console.log(e)
+console.log(e.type)
+console.log(e.target)
+})
+
+const $input = document.getElementById("input1")
+$input.addEventListener("keyup", (e)=>{
+console.log(e)
+console.log(e.type)
+console.log(e.target)
+console.log(e.target.value)
+})
