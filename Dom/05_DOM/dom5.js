b8f9b2bccd88f83e63248976185d73eaae5e0377
matiasecharri <matiasecharri@hotmail.com>
2023-08-28 17:21:05 -0300

    styles

--- Dom/05_DOM/dom5.js
+++ Dom/05_DOM/dom5.js
@@ -1,15 +1,15 @@
 // ⚡72. DOM 12 Events
-const $button = document.getElementById("button1")
-$button.addEventListener("click", (e)=>{
-console.log(e)
-console.log(e.type)
-console.log(e.target)
-})
+const $button = document.getElementById("button1");
+$button.addEventListener("click", e => {
+  console.log(e);
+  console.log(e.type);
+  console.log(e.target);
+});
 
-const $input = document.getElementById("input1")
-$input.addEventListener("keyup", (e)=>{
-console.log(e)
-console.log(e.type)
-console.log(e.target)
-console.log(e.target.value)
-})
+const $input = document.getElementById("input1");
+$input.addEventListener("keyup", e => {
+  console.log(e);
+  console.log(e.type);
+  console.log(e.target);
+  console.log(e.target.value);
+});
