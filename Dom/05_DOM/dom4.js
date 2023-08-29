5978c4df7324fb47e5a3c379ddf2bc06af4ac1e7
matiasecharri <matiasecharri@hotmail.com>
2023-08-28 21:08:04 -0300

    final functionalities

--- Dom/05_DOM/dom4.js
+++ Dom/05_DOM/dom4.js
@@ -1,6 +1,7 @@
 // ⚡72. DOM 12 Events
 const allBlock = () => {
   const $button = document.getElementById("button1");
+  const $buttonMute = document.getElementById("muted");
   const $input = document.getElementById("input1");
   const $textContent = document.getElementById("texter");
   $button.addEventListener("click", e => {
@@ -10,16 +11,28 @@ const allBlock = () => {
   });
 
   let searchBarValue = "";
+  let isMuted = false;
+
+  $buttonMute.addEventListener("click", x => {
+    isMuted === false ? (isMuted = true) : (isMuted = false);
+  });
 
   const playAudioMessage = file => {
     let messageSound = new Audio(file);
-    messageSound.play();
+    if (isMuted === false) {
+      messageSound.play();
+    } else {
+      return;
+    }
   };
 
   const sendButtonInteraction = () => {
     $input.addEventListener("keyup", e => {
       searchBarValue = e.target.value;
     });
+    if ($input.value === "") {
+      return;
+    }
     console.log(searchBarValue);
     $button.addEventListener("click", () => {
       if (searchBarValue.length > 21) {
@@ -44,15 +57,17 @@ const allBlock = () => {
 
   const inputInteraction = () => {
     $input.addEventListener("keydown", e => {
-      if (e.key !== "Enter") {
+      if (e.key !== "Enter" || $input.value === "") {
         return;
       }
+
       if (searchBarValue.length > 21) {
         playAudioMessage("/Dom/05_DOM/error_message.mp3");
         $textContent.innerHTML = `<span class="error4">ERR0R:</span><span class="error3"> Message too long.</span>`;
         $input.value = "";
         return;
       }
+
       let transformedArray = searchBarValue.split("").map(x => {
         if (x.toUpperCase() === "O") {
           return "0";
