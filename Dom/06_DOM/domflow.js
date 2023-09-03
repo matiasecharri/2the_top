a285c13e9973c34968b26f7a7d22c954ff3ba9bf
matiasecharri <matiasecharri@hotmail.com>
2023-09-03 20:45:43 -0300

    Event Delegation - 76

--- Dom/06_DOM/domflow.js
+++ Dom/06_DOM/domflow.js
@@ -5,31 +5,48 @@ const $linkEventos = document.querySelector("#eventos-flujo a");
 console.log($divsEventos);
 console.log($linkEventos);
 
+// ⚡75. DOM 15 stopPropagation & preventDefault
 const flujoEventos = function (e) {
   if (e.target.className === "tres") {
     console.log("3️⃣");
   }
   console.log(
-    `Hola, te saluda ${this.className}, el click lo originó ${e.target.className}`
+    `Hola, te saluda ${e.target.className}, el click lo originó ${e.target.className}`
   );
-  e.stopPropagation(); //Evita que se propague el evento
 };
 
 //Agregando el parametro opcional "TRUE", damos vuelta la burbuja, y entonces pasa expandirse del mas externo al mas interno, se llama fase de captura.
-$divsEventos.forEach(div => {
-  //Fase de burbuja:
-  //   div.addEventListener("click", flujoEventos);
-  //Fase de captura:
-  //   div.addEventListener("click", flujoEventos, true);
-  //Tambien podemos enviar un objeto como tercer parmetro, lo cual nos da más opciones:
-  div.addEventListener("click", flujoEventos, {
-    capture: false,
-    once: false,
-  });
-});
+// $divsEventos.forEach(div => {
+//   //Fase de burbuja:
+//   //   div.addEventListener("click", flujoEventos);
+//   //Fase de captura:
+//   //   div.addEventListener("click", flujoEventos, true);
+//   //Tambien podemos enviar un objeto como tercer parmetro, lo cual nos da más opciones:
+//   div.addEventListener("click", flujoEventos, {
+//     capture: false,
+//     once: false,
+//   });
+// });
 
-$linkEventos.addEventListener("click", e => {
-  alert("Nippon Bitus was Avoided");
-  e.preventDefault(); //Evita la accion por defecto que tenga un elemento, en este caso evita el HREF.
-  e.stopPropagation();
+// $linkEventos.addEventListener("click", e => {
+//   alert("Nippon Bitus was Avoided");
+//   e.preventDefault(); //Evita la accion por defecto que tenga un elemento, en este caso evita el HREF.
+//   e.stopPropagation();
+// });
+
+// $divsEventos.forEach(div => {
+//   div.addEventListener("click", flujoEventos);
+// });
+
+// ⚡76. DOM 16 Event Delegation.
+
+document.addEventListener("click", e => {
+  if (e.target.matches("#eventos-flujo a")) {
+    alert(`event from Document! ${e.target}`);
+    e.preventDefault();
+    return;
+  }
+  if (e.target.matches("#eventos-flujo div")) {
+    flujoEventos(e);
+  }
 });
