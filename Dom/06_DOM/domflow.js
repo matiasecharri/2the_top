f232790840cb8025aae9be78a38542c213dfaf89
matiasecharri <matiasecharri@hotmail.com>
2023-09-03 19:59:00 -0300

    preventDefault() + stopPropagation()

--- Dom/06_DOM/domflow.js
+++ Dom/06_DOM/domflow.js
@@ -1,7 +1,9 @@
 // ⚡74. DOM 14 EVENT FLOW AND BUBBLES
 //Hay dos maneras en como se va propagando un evento, por defecto la propagacion se da desde el elemento mas interno al mas externo, a esto se llama fase de buburja, en el siguiente ejemplo el evento se propaga desde 3 hasta 1.
 const $divsEventos = document.querySelectorAll("#eventos-flujo div");
+const $linkEventos = document.querySelector("#eventos-flujo a");
 console.log($divsEventos);
+console.log($linkEventos);
 
 const flujoEventos = function (e) {
   if (e.target.className === "tres") {
@@ -10,6 +12,7 @@ const flujoEventos = function (e) {
   console.log(
     `Hola, te saluda ${this.className}, el click lo originó ${e.target.className}`
   );
+  e.stopPropagation(); //Evita que se propague el evento
 };
 
 //Agregando el parametro opcional "TRUE", damos vuelta la burbuja, y entonces pasa expandirse del mas externo al mas interno, se llama fase de captura.
@@ -20,7 +23,13 @@ $divsEventos.forEach(div => {
   //   div.addEventListener("click", flujoEventos, true);
   //Tambien podemos enviar un objeto como tercer parmetro, lo cual nos da más opciones:
   div.addEventListener("click", flujoEventos, {
-    capture: true,
-    once: true,
+    capture: false,
+    once: false,
   });
+});
+
+$linkEventos.addEventListener("click", e => {
+  alert("Nippon Bitus was Avoided");
+  e.preventDefault(); //Evita la accion por defecto que tenga un elemento, en este caso evita el HREF.
+  e.stopPropagation();
 });
