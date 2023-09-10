017edacdbbfc528e2838dcf5e38fab8e86bd2d5c
matiasecharri <matiasecharri@hotmail.com>
2023-09-09 22:29:50 -0300

    modularized and bugg fixed

---
+++ Dom/07_DOM_T/01-Menu/app.js
@@ -0,0 +1,8 @@
+import hamburguerMenu from "/Dom/07_DOM_T/01-Menu/hmenu.js";
+
+const $hamburguerIcon = document.getElementById("hamburguerIcon");
+const $menu = document.getElementById("sliderMenu");
+
+hamburguerMenu($hamburguerIcon, $menu);
+
+// Ahora puedes usar "state" como una variable local en este archivo
--- Dom/07_DOM_T/01-Menu/hmenu.js
+++ Dom/07_DOM_T/01-Menu/hmenu.js
@@ -1,16 +1,18 @@
-const $hamburguerIcon = document.getElementById("hamburguerIcon");
-const $menu = document.getElementById("sliderMenu");
-let hamburguerIsOpen = false;
-
-const hamburguerMenu = () => {
-  $hamburguerIcon.addEventListener("click", () => {
-    $menu.classList.toggle("sliderMenuActive");
-    $menu.className.match("sliderMenuActive")
-      ? (hamburguerIsOpen = true)
-      : (hamburguerIsOpen = false);
-    console.log(hamburguerIsOpen);
+let state;
+const $checkbox = document.getElementById("checkbox");
+export default function hamburguerMenu(button, menu) {
+  if (!(button instanceof HTMLElement) || !(menu instanceof HTMLElement)) {
+    console.error("button and menu needs to be a HTML element");
+    return;
+  }
+  window.addEventListener("beforeunload", () => {
+    $checkbox.checked = false;
   });
-};
-hamburguerMenu();
+
+  button.addEventListener("click", () => {
+    menu.classList.toggle("sliderMenuActive");
+    menu.className.match("sliderMenuActive") ? (state = true) : (state = false);
+  });
+}
 
 // https://artlist.io/sfx/pack/cyberpunk/4916?utm_source=google&utm_medium=cpc&utm_campaign=19649240368&utm_content=148830273994&utm_term=&keyword=&ad=650184912426&matchtype=&device=c&gclid=Cj0KCQjwxuCnBhDLARIsAB-cq1pP0Ycjg1L0XLQGSdJcc6ZKBFD4AcB6KUu33K39EBG4AsQEngG10TkaAucFEALw_wcB
