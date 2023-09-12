3f18b319a911aa6ecdf135a57fd407a75f03f00e
matiasecharri <matiasecharri@hotmail.com>
2023-09-12 12:22:12 -0300

    dtitle

--- Dom/07_DOM_T/01-Menu/apiCrypto.js
+++ Dom/07_DOM_T/01-Menu/apiCrypto.js
@@ -6,11 +6,12 @@ export default function apiCrypto() {
 
   let wsBtc = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
   let wsEth = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@trade");
-
+  
   wsBtc.onmessage = event => {
     let stockObject = JSON.parse(event.data);
     console.log(stockObject.p);
     $btcContainer.innerText = `BTC ${parseFloat(stockObject.p).toFixed(2)}`;
+    document.title = `BTC/USDT ${parseFloat(stockObject.p).toFixed(2)}`
   };
   wsEth.onmessage = event => {
     let stockObject = JSON.parse(event.data);
