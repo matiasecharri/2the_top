16ccfaebef50b4c99f4ddf3afa2439975d669d9b
matiasecharri <matiasecharri@hotmail.com>
2023-09-12 02:11:24 -0300

    webssocket

--- Dom/07_DOM_T/01-Menu/apiCrypto.js
+++ Dom/07_DOM_T/01-Menu/apiCrypto.js
@@ -1,6 +1,8 @@
 export default function apiCrypto() {
   const $btcContainer = document.getElementById("btc");
   const $ethContainer = document.getElementById("eth");
+  $btcContainer.innerText = "LOADING...";
+  $ethContainer.innerText = "LOADING...";
 
   let wsBtc = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
   let wsEth = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@trade");
@@ -8,11 +10,11 @@ export default function apiCrypto() {
   wsBtc.onmessage = event => {
     let stockObject = JSON.parse(event.data);
     console.log(stockObject.p);
-    $btcContainer.innerText = parseFloat(stockObject.p).toFixed(2);
+    $btcContainer.innerText = `BTC ${parseFloat(stockObject.p).toFixed(2)}`;
   };
   wsEth.onmessage = event => {
     let stockObject = JSON.parse(event.data);
     console.log(stockObject.p);
-    $ethContainer.innerText = parseFloat(stockObject.p).toFixed(2);
+    $ethContainer.innerText = `ETH ${parseFloat(stockObject.p).toFixed(2)}`;
   };
 }
