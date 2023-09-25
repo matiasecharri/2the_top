7a823d733e0e2d9277b21047315503246a8ac2a5
matiasecharri <matiasecharri@hotmail.com>
2023-09-25 09:49:47 -0300

    adjustements to darkmode

--- Dom/08_FRUITS/app.js
+++ Dom/08_FRUITS/app.js
@@ -5,6 +5,7 @@ const $main = document.querySelector("main");
 const $containerCards = document.getElementById("containerCards");
 const $searchBar = document.getElementById("searchBar");
 const $userPanel = document.getElementById("userPanelX");
+const $fullUserPanel = document.getElementById("userPanel");
 const $led = document.getElementById("ledLight");
 const $goToShoppButton = document.getElementById("goToCartButton");
 const $muteSoundButton = document.getElementById("muteButton");
@@ -240,18 +241,19 @@ const darkMode = () => {
     if (isDarkMode) {
       $filterLayer.classList.add("backdropProviderDark");
       $h1Title.classList.add("backdropProviderDarkH1");
-      $darkModeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      $fullUserPanel.classList.add("darkModeUserPanel"),
+        ($darkModeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z" stroke-width="0" fill="#d7d9d4eb" />
-        </svg>     
-        
-      `;
+        </svg>          
+      `);
     }
   }
   $darkModeButton.addEventListener("click", event => {
     isDarkMode === false
       ? ($filterLayer.classList.add("backdropProviderDark"),
         $h1Title.classList.add("backdropProviderDarkH1"),
+        $fullUserPanel.classList.add("darkModeUserPanel"),
         uiSounds("/Dom/08_FRUITS/assets/sounds/mute-app.wav"),
         (($darkModeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -263,6 +265,7 @@ const darkMode = () => {
       : $filterLayer.classList.remove(
           "backdropProviderDark",
           $h1Title.classList.remove("backdropProviderDarkH1"),
+          $fullUserPanel.classList.remove("darkModeUserPanel"),
           uiSounds("/Dom/08_FRUITS/assets/sounds/mute-app.wav"),
           (($darkModeButton.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-filled" width="24"
           height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
