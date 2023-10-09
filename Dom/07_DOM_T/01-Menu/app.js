440c09062676f803e50435f73cce5340905f5321
matiasecharri <matiasecharri@hotmail.com>
2023-10-09 10:00:10 -0300

    countdown

--- Dom/07_DOM_T/01-Menu/app.js
+++ Dom/07_DOM_T/01-Menu/app.js
@@ -11,6 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
   hamburguerMenu($hamburguerIcon, $menu);
   digitalClock($containerClock);
   digitalAlarm();
-  apiCrypto();
+  // apiCrypto();
   circleMove();
 });
+
+const countDown = (
+  userDate,
+  message = "a really important day",
+  finalMessage = "a beutiful message"
+) => {
+  const $container = document.getElementById("countDownContainer");
+  const targetDate = new Date(userDate).getTime();
+  const actualDate = new Date().getTime();
+  const limitTime = targetDate - actualDate;
+  const daysLeft = Math.floor(limitTime / (1000 * 60 * 60 * 24));
+  const hoursLeft = (
+    "0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  ).slice(-2);
+  const minutesLeft = (
+    "0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))
+  ).slice(-2);
+
+  const secondsLeft = (
+    "0" + Math.floor((limitTime % (1000 * 60)) / 1000)
+  ).slice(-2);
+  $container.innerHTML = `<h4>${message}<br>${daysLeft} days - ${hoursLeft} hours - ${minutesLeft} minutes - ${secondsLeft} seconds </h4>`;
+  let countDownTempo = setInterval(() => {
+    const actualDate = new Date().getTime();
+    const limitTime = targetDate - actualDate;
+    const daysLeft = Math.floor(limitTime / (1000 * 60 * 60 * 24));
+    const hoursLeft = (
+      "0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    ).slice(-2);
+    const minutesLeft = (
+      "0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))
+    ).slice(-2);
+
+    const secondsLeft = (
+      "0" + Math.floor((limitTime % (1000 * 60)) / 1000)
+    ).slice(-2);
+    $container.innerHTML = `<h4>${message}<br>${daysLeft} days - ${hoursLeft} hours - ${minutesLeft} minutes - ${secondsLeft} seconds </h4>`;
+    if (limitTime < 0) {
+      clearInterval(countDownTempo);
+      $container.innerHTML = `<h4> ${finalMessage} </h4>`;
+    }
+  }, 1000);
+};
+
+countDown("Mon Nov 17 2023", "Dibella's Birthday", "Happy Birthday Dibella 🎉");
