ceb32e24992ee644d85b9de591485d23704a6ad4
matiasecharri <matiasecharri@hotmail.com>
2023-10-09 21:31:43 -0300

    deleting useless code

--- Dom/07_DOM_T/01-Menu/d2_clock.js
+++ Dom/07_DOM_T/01-Menu/d2_clock.js
@@ -20,7 +20,6 @@ export function digitalClock(clockTemplate) {
 
   $stopButton.classList.add("disabled");
 
-  let isTheClockRunning = false;
   let clockStart;
   const clockWorking = () => {
     playAudioMessage("/Dom/07_DOM_T/01-Menu/effect1.mp3");
