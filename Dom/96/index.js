43cfb35aeaa526afaa27a0ea1637c3f70a0fcd69
matiasecharri <matiasecharri@hotmail.com>
2023-10-29 21:31:25 -0300

    Up

---
+++ Dom/96/index.js
@@ -0,0 +1,14 @@
+const $searchBar = document.getElementById("searchBar");
+const $container = document.getElementById("container");
+
+const frutas = [
+  { nombre: "Manzana", imagen: "🍎" },
+  { nombre: "Plátano", imagen: "🍌" },
+  { nombre: "Cereza", imagen: "🍒" },
+  { nombre: "Uva", imagen: "🍇" },
+  { nombre: "Fresa", imagen: "🍓" },
+  { nombre: "Piña", imagen: "🍍" },
+  { nombre: "Naranja", imagen: "🍊" },
+  { nombre: "Hambur", imagen: "🍔" },
+  { nombre: "Sushi", imagen: "🍥" },
+];
