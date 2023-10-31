3c7a79d7bd4093864a2f584c97a52703fd3da7c8
matiasecharri <matiasecharri@hotmail.com>
2023-10-31 09:40:01 -0300

    cleaner code + comments

--- Dom/96/index.js
+++ Dom/96/index.js
@@ -1,10 +1,12 @@
+//HTML Elements
 const $cointainer = document.getElementById("container");
 const $searchBar = document.getElementById("searchBar");
 
-const printer = array => {
-  $cointainer.innerHTML = "";
+//PRINTER (Used to print arrays on X container.)
+const printer = (array, container) => {
+  container.innerHTML = "";
   array.forEach(element => {
-    $cointainer.innerHTML += `
+    container.innerHTML += `
     <div class="container-card">
 <img src="${element.photo}" alt="${element.name}">
 <div class="container-card_description">
@@ -16,32 +18,33 @@ const printer = array => {
   });
 };
 
-const searchBar = array => {
+//FILTER (Used to provide functionalities to the input search.)
+const searchBar = (array, container, input) => {
   let userText;
-  $searchBar.addEventListener("keyup", event => {
+  input.addEventListener("keyup", event => {
     userText = event.target.value.toLowerCase();
     console.log(userText);
     const filteredArray = array.filter(element => {
       return element.name.toLowerCase().includes(userText);
     });
     filteredArray.length !== 0
-      ? printer(filteredArray)
-      : ($cointainer.innerHTML = `<p style="color:black">Item not Found</p>`);
+      ? printer(filteredArray, $cointainer)
+      : (container.innerHTML = `<p style="color:black">Item not Found</p>`);
   });
 
-  $searchBar.addEventListener("input", event => {
-    event.target.value === "" ? printer(array) : null;
+  input.addEventListener("input", event => {
+    if (event.target.value === "") printer(array, container);
   });
 };
 
+//API Fetching and function invocation.
 const app = async () => {
   const getData = await fetch(
     "https://my-json-server.typicode.com/matiasecharri/nippon-bites/products"
   );
   const data = await getData.json();
-  console.log(data);
 
-  searchBar(data);
-  printer(data);
+  searchBar(data, $cointainer, $searchBar);
+  printer(data, $cointainer);
 };
 app();
