becac7087553f33eabb5e00eefd3061e7887e772
matiasecharri <matiasecharri@hotmail.com>
2023-10-31 09:56:57 -0300

    fragment

--- Dom/96/index.js
+++ Dom/96/index.js
@@ -4,18 +4,51 @@ const $searchBar = document.getElementById("searchBar");
 
 //PRINTER (Used to print arrays on X container.)
 const printer = (array, container) => {
-  container.innerHTML = "";
+  // Fragment to construct elements
+  const fragment = document.createDocumentFragment();
+
   array.forEach(element => {
-    container.innerHTML += `
-    <div class="container-card">
-<img src="${element.photo}" alt="${element.name}">
-<div class="container-card_description">
-    <h2>${element.name}</h2>
-    <p>Price per 5 unit: ${element.pricex5} USD.-</p>
-    <p>${element.description}</p>
-</div>
-</div>`;
+    //Div creation
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "container-card";
+
+    //Img creation
+    const img = document.createElement("img");
+    img.src = element.photo;
+    img.alt = element.name;
+
+    //Description container
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "container-card_description";
+
+    //h2 and p creation
+    const h2 = document.createElement("h2");
+    h2.textContent = element.name;
+
+    const priceParagraph = document.createElement("p");
+    priceParagraph.textContent = `Price per 5 unit: ${element.pricex5} USD.-`;
+
+    const descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = element.description;
+
+    // Adding elements to description container
+    descriptionDiv.appendChild(h2);
+    descriptionDiv.appendChild(priceParagraph);
+    descriptionDiv.appendChild(descriptionParagraph);
+
+    // Adding image and description to main card
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(descriptionDiv);
+
+    // Adding card to fragment
+    fragment.appendChild(cardDiv);
   });
+
+  // Limpiar el contenido del contenedor
+  container.innerHTML = "";
+
+  // Adjuntar el fragmento completo al contenedor
+  container.appendChild(fragment);
 };
 
 //FILTER (Used to provide functionalities to the input search.)
