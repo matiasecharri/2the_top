ddf118ac2d6fc458b1fb28b78b6b6a29b2184593
matiasecharri <matiasecharri@hotmail.com>
2023-11-02 20:16:56 -0300

    todo process

--- Dom/96.5/index.js
+++ Dom/96.5/index.js
@@ -2,6 +2,8 @@
 const $inputText = document.getElementById("textField");
 const $buttonSend = document.getElementById("send");
 const $containerList = document.getElementById("containerToDo");
+const $whiteButton = document.getElementById("whitePoint");
+const $containerrAll = document.querySelector(".container");
 
 const toDoArray = [];
 
@@ -19,14 +21,19 @@ const printer = array => {
   $containerList.innerHTML = "";
   array.forEach(element => {
     $containerList.innerHTML += `
+    <div class="container-item">
     <div class="item" id="${element.id}">
     <p>"${element.content}"</p>
+</div>
 </div>`;
   });
 };
 
 const toDoCreator = () => {
   $buttonSend.addEventListener("click", event => {
+    if ($inputText.value === "") {
+      return;
+    }
     const userInput = $inputText.value;
     inputText(userInput);
     $inputText.value = "";
@@ -35,4 +42,24 @@ const toDoCreator = () => {
   });
 };
 
+const toDoCreatorKey = () => {
+  $inputText.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+      const userInput = $inputText.value;
+      inputText(userInput);
+      $inputText.value = "";
+      console.log(toDoArray);
+      printer(toDoArray);
+    }
+  });
+};
+
+const widthChanger = () => {
+  $whiteButton.addEventListener("click", event => {
+    $containerrAll.classList.toggle("expanded");
+  });
+};
+
+toDoCreatorKey();
 toDoCreator();
+widthChanger();
