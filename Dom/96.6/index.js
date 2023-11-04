7f9bae5c1f74864ef816c3812e8ad700ce3ecf31
matiasecharri <matiasecharri@hotmail.com>
2023-11-04 01:12:25 -0300

    fancy stuff

--- Dom/96.6/index.js
+++ Dom/96.6/index.js
@@ -1,7 +1,13 @@
 const $inputText = document.getElementById("textField");
 const $buttonSend = document.getElementById("send");
 const $containerToDos = document.getElementById("scroll-content");
+const $whiteButton = document.getElementById("whitePoint");
+const $whiteModal = document.getElementById("whiteModal");
+const $darkLightButton = document.getElementById("themeChanger");
+const $main = document.getElementById("main");
+const $bgImage = document.getElementById("bgImage");
 
+let isDarkMode = false;
 const arrayToDos = [];
 
 const printer = array => {
@@ -44,5 +50,29 @@ const toDoRenderKey = () => {
   });
 };
 
+const appearWhiteModal = () => {
+  $whiteButton.addEventListener("click", event => {
+    $whiteModal.classList.toggle("modalAppear");
+  });
+};
+
+const themeChecker = () => {
+  isDarkMode === false
+    ? ($main.classList.add("darkMain"),
+      $bgImage.classList.add("darkImage"),
+      (isDarkMode = true))
+    : ($main.classList.remove("darkMain"),
+      $bgImage.classList.remove("darkImage"),
+      (isDarkMode = false));
+};
+
+const buttonDarkLight = () => {
+  $darkLightButton.addEventListener("click", event => {
+    themeChecker();
+  });
+};
+
 toDoRenderKey();
 toDoRender();
+appearWhiteModal();
+buttonDarkLight();
