6f182907f2d0f2fcabc8c52118dfe4751bb91419
matiasecharri <matiasecharri@hotmail.com>
2023-11-25 20:16:17 -0300

    mockup

---
+++ zCRUDAJAX/script.js
@@ -0,0 +1,7 @@
+const d = document;
+const $table = d.querySelector("content-characters");
+const $form = d.querySelector(".crud-form");
+const $title = d.querySelector(".crud-title");
+const $template = d.getElementById("crud-template").content;
+const $fragment = document.createDocumentFragment();
+console.log($template);
