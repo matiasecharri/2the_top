83fb2ccf08473ee4b7d497957a81369f5652a860
matiasecharri <matiasecharri@hotmail.com>
2023-12-03 17:01:17 -0300

    printing

--- zCRUDAJAX/scriptXMLHttp.js
+++ zCRUDAJAX/scriptXMLHttp.js
@@ -5,6 +5,67 @@ const $title = d.querySelector(".crud-title");
 const $sendButton = d.getElementById("send");
 const $fragment = d.createDocumentFragment();
 
+const printing = array => {
+  if (array.length === 0) {
+    return;
+  }
+  array.forEach(character => {
+    const $card = d.createElement("div");
+    $card.classList.add("card");
+    $card.classList.add("card-help");
+    const $overlay = d.createElement("div");
+
+    const $image = d.createElement("img");
+    $image.setAttribute("src", character.photo);
+    $image.setAttribute("alt", `A photo of ${character.name}.`);
+    $image.onerror = () => {
+      console.warn(
+        `${character.name} photo has a problem and is loading the default picture.`
+      );
+      $image.setAttribute("src", "/zCRUDAJAX/failedToLoad.jpg");
+    };
+    const $cardInfo = d.createElement("div");
+    $cardInfo.classList.add("card-info");
+
+    const $characterTitle = d.createElement("h2");
+    $characterTitle.innerText = character.name;
+
+    const $p1 = d.createElement("p");
+    const $p2 = d.createElement("p");
+    let cromo;
+    let alive;
+    character.hasCromo === "true" ? (cromo = "Yes") : (cromo = "No");
+    character.isAlive === "true" ? (alive = "Yes") : (alive = "No");
+    if (character.isAlive === "null") {
+      alive = "Maybe...";
+    }
+    $p1.innerText = `Cromo: ${cromo} - Is Alive: ${alive}`;
+    $p2.innerText = character.info;
+
+    const $buttonsDiv = d.createElement("div");
+    $buttonsDiv.classList.add("card-buttons");
+    const $buttonEdit = d.createElement("button");
+    const $buttonDelete = d.createElement("button");
+    $buttonEdit.innerText = "EDIT";
+    $buttonDelete.innerText = "DELETE";
+    $buttonDelete.classList.add(character.id);
+    $buttonDelete.classList.add("delete");
+
+    $card.appendChild($image);
+    $card.appendChild($overlay);
+    $cardInfo.appendChild($characterTitle);
+    $cardInfo.appendChild($p1);
+    $cardInfo.appendChild($p2);
+    $card.appendChild($cardInfo);
+    $buttonsDiv.appendChild($buttonEdit);
+    $buttonsDiv.appendChild($buttonDelete);
+    $card.appendChild($buttonsDiv);
+    $fragment.appendChild($card);
+  });
+  $table.innerHTML = "";
+  $table.appendChild($fragment);
+};
+
 const ajax = options => {
   let { url, method, success, error, data } = options;
 
@@ -33,12 +94,20 @@ const getAllCharacters = () => {
     success: res => {
       console.log(res);
       const characters = res;
-      characters.forEach(element => {
-        console.log(element);
-      });
+      printing(characters);
     },
     error: err => {
       console.warn(err);
+      document.title = "⚠️ERROR!";
+      const $errorOnDocument = document.createElement("p");
+      $errorOnDocument.innerText = err.toUpperCase();
+      $table.style.setProperty("justify-content", "center");
+      $table.style.setProperty("align-items", "center");
+      $errorOnDocument.style.setProperty("font-size", "3rem");
+      $errorOnDocument.style.setProperty("color", "red");
+      $table.appendChild($errorOnDocument);
+      $title.style.setProperty("color", "red");
+      $title.innerText = "⚠️ERROR SOMETHING HAPPEND";
     },
     data: null, //Not needed
   });
