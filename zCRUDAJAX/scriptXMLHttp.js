5ffaf13bd4f5e9d5aeb6e7740dfbfbc67b2c511b
matiasecharri <matiasecharri@hotmail.com>
2023-12-03 22:15:24 -0300

    crud XMLHTTP finished

--- zCRUDAJAX/scriptXMLHttp.js
+++ zCRUDAJAX/scriptXMLHttp.js
@@ -4,9 +4,13 @@ const $form = d.querySelector(".crud-form");
 const $title = d.querySelector(".crud-title");
 const $sendButton = d.getElementById("send");
 const $h1 = d.querySelector("h1");
+let isEditing = false;
+let idToEdit;
+let dataAll;
 $h1.innerText = "EDGERUNNERS CRUD API REST AJAX XMLHTTP";
 const $fragment = d.createDocumentFragment();
 
+//Printer function
 const printing = array => {
   if (array.length === 0) {
     return;
@@ -75,6 +79,7 @@ const printing = array => {
   $table.appendChild($fragment);
 };
 
+//Ajax configuration
 const ajax = options => {
   let { url, method, success, error, data } = options;
 
@@ -96,6 +101,7 @@ const ajax = options => {
   request.send(JSON.stringify(data));
 };
 
+//GET METHOD
 const getAllCharacters = () => {
   ajax({
     url: "http://localhost:5000/cyberpunk-characters",
@@ -103,6 +109,7 @@ const getAllCharacters = () => {
     success: json => {
       console.log(json);
       const characters = json;
+      dataAll = characters;
       printing(characters);
     },
     error: err => {
@@ -122,4 +129,164 @@ const getAllCharacters = () => {
   });
 };
 
+//DELETE METHOD
+const deleteCharacter = id => {
+  ajax({
+    url: `http://localhost:5000/cyberpunk-characters/${id}`,
+    method: "DELETE", //If the user doesnt specifies a method is going to apply GET as default (line 24)
+    success: json => {
+      console.log(json);
+      const characters = json;
+      printing(characters);
+    },
+    error: err => {
+      console.warn(err);
+      document.title = "⚠️ERROR!";
+      const $errorOnDocument = document.createElement("p");
+      $errorOnDocument.innerText = err.toUpperCase();
+      $table.style.setProperty("justify-content", "center");
+      $table.style.setProperty("align-items", "center");
+      $errorOnDocument.style.setProperty("font-size", "3rem");
+      $errorOnDocument.style.setProperty("color", "red");
+      $table.appendChild($errorOnDocument);
+      $title.style.setProperty("color", "red");
+      $title.innerText = "⚠️ERROR SOMETHING HAPPEND";
+    },
+  });
+};
+
+//PATCH METHOD
+const editCharacter = id => {
+  ajax({
+    url: `http://localhost:5000/cyberpunk-characters/${id}`,
+    method: "PATCH", //If the user doesnt specifies a method is going to apply GET as default (line 24)
+    success: json => {
+      console.log(json);
+      const characters = json;
+      printing(characters);
+    },
+    error: err => {
+      console.warn(err);
+      document.title = "⚠️ERROR!";
+      const $errorOnDocument = document.createElement("p");
+      $errorOnDocument.innerText = err.toUpperCase();
+      $table.style.setProperty("justify-content", "center");
+      $table.style.setProperty("align-items", "center");
+      $errorOnDocument.style.setProperty("font-size", "3rem");
+      $errorOnDocument.style.setProperty("color", "red");
+      $table.appendChild($errorOnDocument);
+      $title.style.setProperty("color", "red");
+      $title.innerText = "⚠️ERROR SOMETHING HAPPEND";
+    },
+    data: {
+      id: id,
+      name: $form[0].value,
+      info: $form[1].value,
+      isAlive: $form[2].value,
+      hasCromo: $form[3].value,
+      photo: $form[4].value,
+    },
+  });
+};
+
+//POST METHOD
+const createCharacter = id => {
+  ajax({
+    url: `http://localhost:5000/cyberpunk-characters`,
+    method: "POST", //If the user doesnt specifies a method is going to apply GET as default (line 24)
+    success: json => {
+      console.log(json);
+      const characters = json;
+      printing(characters);
+    },
+    error: err => {
+      console.warn(err);
+      document.title = "⚠️ERROR!";
+      const $errorOnDocument = document.createElement("p");
+      $errorOnDocument.innerText = err.toUpperCase();
+      $table.style.setProperty("justify-content", "center");
+      $table.style.setProperty("align-items", "center");
+      $errorOnDocument.style.setProperty("font-size", "3rem");
+      $errorOnDocument.style.setProperty("color", "red");
+      $table.appendChild($errorOnDocument);
+      $title.style.setProperty("color", "red");
+      $title.innerText = "⚠️ERROR SOMETHING HAPPEND";
+    },
+    data: {
+      id: id,
+      name: $form[0].value,
+      info: $form[1].value,
+      isAlive: $form[2].value,
+      hasCromo: $form[3].value,
+      photo: $form[4].value,
+    },
+  });
+};
+
+//Actions in the delete button
+const buttonDeleteActions = () => {
+  $table.addEventListener("click", event => {
+    if (event.target.classList.contains("delete")) {
+      const idToDelete = event.target.dataset.id;
+      console.log(idToDelete);
+      deleteCharacter(idToDelete);
+    }
+  });
+};
+
+//Actions in the edit button
+const buttonEditActions = () => {
+  $table.addEventListener("click", event => {
+    if (event.target.classList.contains("edit")) {
+      if (isEditing === true) {
+        $form[0].value = "";
+        $form[1].value = "";
+        $form[2].value = "";
+        $form[3].value = "";
+        $form[4].value = "";
+        const buttonsEdit = document.querySelectorAll(".edit");
+        buttonsEdit.forEach(button => {
+          button.innerText = "EDIT";
+        });
+        isEditing = false;
+        return;
+      }
+      $title.innerText = "EDIT MODE";
+      idToEdit = event.target.dataset.id;
+      $form[0].value = event.target.dataset.name;
+      $form[1].value = event.target.dataset.info;
+      $form[2].value = event.target.dataset.isAlive;
+      $form[3].value = event.target.dataset.hasCromo;
+      $form[4].value = event.target.dataset.photo;
+      const buttonsEdit = document.querySelectorAll(".edit");
+      buttonsEdit.forEach(button => {
+        button.innerText = "CANCEL";
+      });
+      isEditing = true;
+    }
+  });
+};
+
+$sendButton.addEventListener("click", event => {
+  event.preventDefault();
+  if (isEditing === false) {
+    return;
+  }
+  editCharacter(idToEdit);
+});
+
+$sendButton.addEventListener("click", event => {
+  event.preventDefault();
+  if (isEditing === true) {
+    return;
+  }
+  const arrayId = dataAll.map(character => {
+    return character.id;
+  });
+
+  const newId = Math.max(...arrayId) + 1;
+  createCharacter(newId);
+});
+buttonEditActions();
+buttonDeleteActions();
 d.addEventListener("DOMContentLoaded", getAllCharacters);
