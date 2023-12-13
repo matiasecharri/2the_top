655a6deb3b8a69aa896e3f231cec0ec6ae98012c
matiasecharri <matiasecharri@hotmail.com>
2023-12-12 23:26:05 -0300

    up

--- v.js
+++ v.js
@@ -2,9 +2,21 @@ const $containerCards = document.querySelector(".main-card-container");
 const $searchBar = document.getElementById("searchbar");
 const $containerCheckboxes = document.getElementById("checkbox-container");
 const $fragment = document.createDocumentFragment();
-const checkedCategories = [];
+const activeCategories = [];
+let checkeds = [];
 
-const printer = array => {
+const discardErrors = array => {
+  const filtered = array.filter(character => character.role !== null);
+  return filtered;
+};
+
+const generateCategories = array => {
+  const repeatedCategories = array.map(character => character.role.displayName);
+  const unrepeatedCategories = [...new Set(repeatedCategories)];
+  return unrepeatedCategories;
+};
+
+const renderCharacters = array => {
   $containerCards.innerHTML = "";
   array.forEach(agent => {
     const $card = document.createElement("div");
@@ -35,119 +47,87 @@ const printer = array => {
   $containerCards.appendChild($fragment);
 };
 
-const searchingBrokenAgents = array => {
-  const filteredArray = array.filter(agent => agent.role !== null);
-  return filteredArray;
-};
-
-const createCheckboxList = array => {
-  const roles = array.map(agent => agent.role.displayName);
-  const unrepeatedList = [...new Set(roles)];
-  return unrepeatedList;
-};
-
 const renderCheckbox = array => {
   array.forEach(category => {
-    const $checkBox = document.createElement("input");
-    $checkBox.type = "checkbox";
-    $checkBox.dataset.id = category;
-
     const $label = document.createElement("label");
-    $label.classList.add("fix-style");
-    $label.textContent = category;
-
-    const $fragment = document.createDocumentFragment();
-    $fragment.appendChild($checkBox);
+    $label.innerText = category;
+    const $checkbox = document.createElement("input");
+    $checkbox.type = "checkbox";
+    $checkbox.dataset.id = category;
+    $label.appendChild($checkbox);
     $fragment.appendChild($label);
-
-    $containerCheckboxes.appendChild($fragment);
   });
+  $containerCheckboxes.appendChild($fragment);
 };
 
-const checkActions = array => {
-  $containerCheckboxes.addEventListener("click", event => {
+const checkboxActions = array => {
+  $containerCheckboxes.addEventListener("change", event => {
+    event.preventDefault();
     if (event.target.matches("input")) {
-      if (event.target.checked === true) {
-        if (!checkedCategories.includes(event.target.dataset.id))
-          checkedCategories.push(event.target.dataset.id.toLowerCase());
-        const arrayChequeados = array.filter(agent => {
-          if (
-            checkedCategories.includes(agent.role.displayName.toLowerCase())
-          ) {
-            return agent;
-          }
-        });
-        if (arrayChequeados.length !== 0) {
-          printer(arrayChequeados);
-        }
-      }
-
-      if (event.target.checked === false) {
-        const categoryToDelete = checkedCategories.indexOf(
-          event.target.dataset.id
+      if (!activeCategories.includes(event.target.dataset.id)) {
+        activeCategories.push(event.target.dataset.id);
+      } else {
+        const categoryToDelete = activeCategories.findIndex(
+          category =>
+            category === event.target.dataset.id &&
+            event.target.checked === false
         );
-        checkedCategories.splice(categoryToDelete, 1);
-        const arrayChequeados = array.filter(agent => {
-          if (
-            checkedCategories.includes(agent.role.displayName.toLowerCase())
-          ) {
-            return agent;
-          }
-        });
-        if (arrayChequeados.length !== 0) {
-          printer(arrayChequeados);
-        } else {
-          printer(array);
-        }
+        activeCategories.splice(categoryToDelete, 1);
       }
-    }
-  });
-};
-
-const crossedFilter = array => {
-  let userText;
-  $searchBar.addEventListener("keyup", event => {
-    userText = event.target.value.toLowerCase();
-    const filteredText = array.filter(agent => {
-      return agent.displayName.toLowerCase().includes(userText.toLowerCase());
-    });
-    if (checkedCategories.length === 0) {
-      printer(filteredText);
-    } else {
-      const filteredTextAndChecks = array.filter(agent => {
-        if (
-          checkedCategories.includes(agent.role.displayName.toLowerCase()) &&
-          agent.displayName.toLowerCase().includes(userText.toLowerCase())
-        ) {
-          return agent;
+      const arrayCheckeds = array.filter(character => {
+        if (activeCategories.includes(character.role.displayName)) {
+          return character;
         }
       });
-      printer(filteredTextAndChecks);
+      if (activeCategories.length !== 0) {
+        checkeds = arrayCheckeds;
+        renderCharacters(arrayCheckeds);
+      } else {
+        renderCharacters(array);
+        checkeds = arrayCheckeds;
+      }
     }
   });
 };
 
-const getData = async () => {
+const searchBarActions = array => {
+  let userText;
+  $searchBar.addEventListener("input", event => {
+    userText = event.target.value.toLowerCase();
+    if (activeCategories.length !== 0) {
+      const filteredAndChecked = checkeds.filter(character => {
+        return character.displayName.toLowerCase().includes(userText);
+      });
+      renderCharacters(filteredAndChecked);
+    } else {
+      const filteredArray = array.filter(character => {
+        return character.displayName.toLowerCase().includes(userText);
+      });
+      renderCharacters(filteredArray);
+    }
+  });
+};
+
+const consumeApi = async () => {
   try {
     const response = await fetch("https://valorant-api.com/v1/agents");
     if (!response.ok) {
       throw {
-        message:
-          response.statusText || "Something went wrong, cant get the agents!",
+        message: response.statusText || "Something went wrong...",
         status: response.status,
       };
     }
     const data = await response.json();
-    const agents = data.data;
-    const newArray = searchingBrokenAgents(agents);
-    const checkboxes = createCheckboxList(newArray);
-    printer(newArray);
-    renderCheckbox(checkboxes);
-    checkActions(newArray);
-    crossedFilter(newArray);
+    const agentsBugged = data.data;
+    const agents = discardErrors(agentsBugged);
+    const categories = generateCategories(agents);
+    renderCheckbox(categories);
+    renderCharacters(agents);
+    checkboxActions(agents);
+    searchBarActions(agents);
   } catch (error) {
     console.warn(error);
   }
 };
 
-getData();
+consumeApi();
