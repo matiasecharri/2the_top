08f6ff8a03e1fd8fc504b817ac123c5eb83cddbe
matiasecharri <matiasecharri@hotmail.com>
2024-03-19 21:19:08 -0300

    feat: upgraded code + alt option

--- zyAJAX-EXERCISES/AJAXStripe/stripe.js
+++ zyAJAX-EXERCISES/AJAXStripe/stripe.js
@@ -1,8 +1,56 @@
 import KEYS from "./stripekeys.js";
 import BATMAN from "./trash.js";
 
-const publicK = KEYS.public;
-const secretK = KEYS.secret;
-console.table(publicK, secretK);
+const d = document;
+const $foodContainer = d.querySelector("main");
+const $template = d.querySelector(".template-food").content;
+const $fragment = d.createDocumentFragment();
 
+const options = {
+  headers: {
+    Authorization: `Bearer ${KEYS.secret}`,
+  },
+};
+
+const errorHandler = (res, res2) => {
+  if (!res.ok) {
+    throw new Error(
+      `🚬 Something went wrong with the 🛍️PRODUCTS, ERROR: ${res.status} 
+           👉 More info: https://developer.mozilla.org/es/docs/Web/HTTP/Status`
+    );
+  }
+
+  if (!res2.ok) {
+    throw new Error(
+      `🚬 Something went wrong with the 💸PRICES, ERROR: ${res2.status} 
+             👉 More info: https://developer.mozilla.org/es/docs/Web/HTTP/Status`
+    );
+  }
+};
+
+const getData = async () => {
+  try {
+    const [res, res2] = await Promise.all([
+      fetch("https://api.stripe.com/v1/products", options),
+      fetch("https://api.stripe.com/v1/prices", options),
+    ]);
+
+    errorHandler(res, res2);
+
+    const [dataProducts, dataPrices] = await Promise.all([
+      res.json(),
+      res2.json(),
+    ]);
+
+    const products = dataProducts.data;
+    const prices = dataPrices.data;
+
+    console.log(products);
+    console.log(prices);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getData();
 BATMAN();
---
+++ zyAJAX-EXERCISES/AJAXStripe/stripeThen.js
@@ -0,0 +1,16 @@
+const getDataThen = () => {
+  fetch("https://api.stripe.com/v1/products", options)
+    .then(res =>
+      res.ok
+        ? res.json()
+        : Promise.reject(`Something went wrong: ${res.status}`)
+    )
+    .then(json => {
+      console.log(json);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+getDataThen();
