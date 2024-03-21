24db67a900d7e7eb933c0fe46fbb237e85971f68
matiasecharri <matiasecharri@hotmail.com>
2024-03-20 21:25:52 -0300

    feat: renderer added

--- zyAJAX-EXERCISES/AJAXStripe/stripe.js
+++ zyAJAX-EXERCISES/AJAXStripe/stripe.js
@@ -14,15 +14,14 @@ const optionsSec = {
 const errorHandler = (res, res2) => {
   if (!res.ok) {
     throw new Error(
-      `🚬 Something went wrong with the 🛍️PRODUCTS, ERROR: ${res.status}
-       👉 More info: https://developer.mozilla.org/es/docs/Web/HTTP/Status`
-    );
-  }
-
-  if (!res2.ok) {
-    throw new Error(
       `🚬 Something went wrong with the 💸PRICES, ERROR: ${res2.status} 
              👉 More info: https://developer.mozilla.org/es/docs/Web/HTTP/Status`
+    );
+  }
+  if (!res2.ok) {
+    throw new Error(
+      `🚬 Something went wrong with the 🛍️PRODUCTS, ERROR: ${res.status}
+       👉 More info: https://developer.mozilla.org/es/docs/Web/HTTP/Status`
     );
   }
 };
@@ -31,43 +30,48 @@ const moneyFormat = string => {
   return `$${string.slice(0, -2)}.${string.slice(-2)}`;
 };
 
+const productRendering = (arrayPrices, arrayProducts) => {
+  arrayPrices.forEach(price => {
+    const productData = arrayProducts.filter(
+      product => product.id === price.product
+    );
+    console.log(productData);
+    $template.querySelector(".card-food").setAttribute("data-price", price.id);
+    $template.querySelector("img").src = productData[0].images[0];
+    $template.querySelector("img").alt = productData[0].name;
+    $template.querySelector("h3").innerText = productData[0].name;
+    $template.querySelector(".card-food_text-description").innerText =
+      productData[0].description;
+    $template.querySelector(
+      ".card-food_text-price"
+    ).innerText = `Price: ${moneyFormat(
+      price.unit_amount_decimal
+    )} ${price.currency.toUpperCase()}`;
+
+    const $clone = d.importNode($template, true);
+    $fragment.appendChild($clone);
+  });
+  $main.appendChild($fragment);
+};
+
 const getData = async () => {
   try {
-    const [res, res2] = await Promise.all([
-      fetch("https://api.stripe.com/v1/products", optionsSec),
+    const [resPrices, resProducts] = await Promise.all([
       fetch("https://api.stripe.com/v1/prices", optionsSec),
+      fetch("https://api.stripe.com/v1/products", optionsSec),
     ]);
 
-    errorHandler(res, res2);
+    errorHandler(resPrices, resProducts);
 
-    const [dataProducts, dataPrices] = await Promise.all([
-      res.json(),
-      res2.json(),
+    const [jsonPrices, jsonProducts] = await Promise.all([
+      resPrices.json(),
+      resProducts.json(),
     ]);
 
-    const products = dataProducts.data;
-    const prices = dataPrices.data;
-    prices.forEach(price => {
-      const productData = products.filter(
-        product => product.id === price.product
-      );
-      console.log(productData);
-      $template.querySelector("img").src = productData[0].images[0];
-      $template.querySelector("img").alt = productData[0].name;
-      $template.querySelector("h3").innerText = productData[0].name;
-      $template.querySelector(".card-food_text-description").innerText =
-        productData[0].description;
-      $template.querySelector(
-        ".card-food_text-price"
-      ).innerText = `Price: ${moneyFormat(
-        price.unit_amount_decimal
-      )} ${price.currency.toUpperCase()}`;
+    const prices = jsonPrices.data;
+    const products = jsonProducts.data;
 
-      const $clone = d.importNode($template, true);
-      $fragment.appendChild($clone);
-    });
-
-    $main.appendChild($fragment);
+    productRendering(prices, products);
   } catch (err) {
     console.error(err);
     $main.innerHTML = `<p class="err-message"> ${err} </p>`;
