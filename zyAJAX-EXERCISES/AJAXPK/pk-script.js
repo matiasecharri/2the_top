e3631de2c494c9d7b9a0bbf9cfdff25e9a8e1f61
matiasecharri <matiasecharri@hotmail.com>
2024-03-25 07:23:13 -0300

    feat: skeleton added

--- zyAJAX-EXERCISES/AJAXPK/pk-script.js
+++ zyAJAX-EXERCISES/AJAXPK/pk-script.js
@@ -1,28 +1,54 @@
-const d = document
-const $templateCard =  d.querySelector(".template-pk").content;
+const d = document;
+const $templateCard = d.querySelector(".template-pk").content;
 const $fragment = d.createDocumentFragment();
-const $main = d.querySelector("main")
-const $nextButton = document.getElementById("next")
-const $prevButton = document.getElementById("prev")
+const $main = d.querySelector("main");
+const $nextButton = document.getElementById("next");
+const $prevButton = document.getElementById("prev");
 
-let offset = 0
-let limit
-const offsetIncrease = 20
+let offset = 0;
+let limit;
+const offsetIncrease = 20;
 
 const renderCards = (array, container) => {
-array.forEach(pokemon => {
-    const $img = $templateCard.querySelector(".card_img")
-    const $title = $templateCard.querySelector(".card_title")
-    $img.src = pokemon.sprites.front_default
-    $img.alt = pokemon.name
-    $title.innerText = `${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}`
+  array.forEach((pokemon) => {
+    const $img = $templateCard.querySelector(".card_img");
+    const $title = $templateCard.querySelector(".card_title");
+    $img.src = pokemon.sprites.front_default;
+    $img.alt = pokemon.name;
+    $title.innerText = `${
+      pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    }`;
 
     const $clone = d.importNode($templateCard, true);
     $fragment.appendChild($clone);
-})
-container.innerHTML = ""
-container.appendChild($fragment)
-}
+  });
+  setTimeout(() => {
+    container.innerHTML = "";
+    container.appendChild($fragment);
+  }, 550);
+};
+
+const renderSkeletons = () => {
+  const skeletons = new Array(offsetIncrease).fill(undefined);
+  const $skeletonsHTML = skeletons.map(
+    () => `
+  <div class="skeleton">
+    <div class="skeleton-img"></div>
+    <div class="skeleton-details">
+      <div class="skeleton-line"></div>
+      <div class="skeleton-line"></div>
+      <div class="skeleton-line"></div>
+      <div class="skeleton-line"></div>
+    </div>
+  </div>
+`
+  );
+  $main.innerHTML = $skeletonsHTML
+};
+
+const renderLoader = () => {
+  $main.innerHTML = `<div class="loader"></div>`;
+};
 
 const getData = async () => {
   try {
@@ -32,48 +58,48 @@ const getData = async () => {
     if (!res.ok) {
       throw new Error(`${res.status} First fetch failed 🚬`);
     }
-    
+
     const data = await res.json();
-    limit = data.count
+    limit = data.count;
     const pokemons = data.results;
     /*🚀 General fetch finished 🚀*/
 
-    const individualURL = pokemons.map(async pokemon => {
-        try {
-            const url = await fetch(pokemon.url)
-            if(!url.ok){
-                throw new Error(`${url.status} Individual fetch failed 🚬`)
-            }
-            return url.json()
-        } catch (err) {
-            console.warn(err)
+    const individualURL = pokemons.map(async (pokemon) => {
+      try {
+        const url = await fetch(pokemon.url);
+        if (!url.ok) {
+          throw new Error(`${url.status} Individual fetch failed 🚬`);
         }
-    })
+        return url.json();
+      } catch (err) {
+        console.warn(err);
+      }
+    });
 
-   const individualInfo = await Promise.all(individualURL)
-   console.log(individualInfo)
-   renderCards(individualInfo, $main)
-
+    const individualInfo = await Promise.all(individualURL);
+    console.log(individualInfo);
+    renderCards(individualInfo, $main);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 };
 
 getData();
 
+$nextButton.addEventListener("click", (event) => {
+  if (offset >= limit - offsetIncrease) {
+    return;
+  }
+  renderSkeletons();
+  offset += offsetIncrease;
+  getData();
+});
 
-$nextButton.addEventListener("click", event => {
-    if(offset >= limit - offsetIncrease){
-        return
-    }
-    offset += offsetIncrease
-    getData()
-})
-
-$prevButton.addEventListener("click", event => {
-    if (offset === 0){
-        return
-    }
-    offset -= offsetIncrease
-    getData()
-})
+$prevButton.addEventListener("click", (event) => {
+  if (offset === 0) {
+    return;
+  }
+  renderSkeletons();
+  offset -= offsetIncrease;
+  getData();
+});
