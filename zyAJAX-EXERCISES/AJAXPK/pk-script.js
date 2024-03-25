f168820a5e421bf793163fba8e42d2245d7c6545
matiasecharri <matiasecharri@hotmail.com>
2024-03-25 06:30:40 -0300

    feat: offset, limit, offsetIncrease

--- zyAJAX-EXERCISES/AJAXPK/pk-script.js
+++ zyAJAX-EXERCISES/AJAXPK/pk-script.js
@@ -1,24 +1,58 @@
+const d = document
+const $templateCard =  d.querySelector(".template-pk").content;
+const $fragment = d.createDocumentFragment();
+const $main = d.querySelector("main")
+const $nextButton = document.getElementById("next")
+const $prevButton = document.getElementById("prev")
+
+let offset = 0
+let limit
+const offsetIncrease = 20
+
+const renderCards = (array, container) => {
+array.forEach(pokemon => {
+    const $img = $templateCard.querySelector(".card_img")
+    const $title = $templateCard.querySelector(".card_title")
+    $img.src = pokemon.sprites.front_default
+    $img.alt = pokemon.name
+    $title.innerText = `${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}`
+
+    const $clone = d.importNode($templateCard, true);
+    $fragment.appendChild($clone);
+})
+container.innerHTML = ""
+container.appendChild($fragment)
+}
+
 const getData = async () => {
   try {
     const res = await fetch(
-      "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0"
+      `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`
     );
     if (!res.ok) {
-      throw new Error("First fetch failed");
+      throw new Error(`${res.status} First fetch failed 🚬`);
     }
+    
     const data = await res.json();
+    limit = data.count
     const pokemons = data.results;
+    /*🚀 General fetch finished 🚀*/
 
-    const pokemonsPromises = pokemons.map(async (pokemon) => {
-        const url = await fetch(pokemon.url)
-        if(!url.ok){
-            throw new Error(url.status + url.statusText)
+    const individualURL = pokemons.map(async pokemon => {
+        try {
+            const url = await fetch(pokemon.url)
+            if(!url.ok){
+                throw new Error(`${url.status} Individual fetch failed 🚬`)
+            }
+            return url.json()
+        } catch (err) {
+            console.warn(err)
         }
-        return url.json()
     })
 
-    const individualPokemonData = await Promise.all(pokemonsPromises)
-    console.log(individualPokemonData)
+   const individualInfo = await Promise.all(individualURL)
+   console.log(individualInfo)
+   renderCards(individualInfo, $main)
 
   } catch (err) {
     console.error(err)
@@ -26,3 +60,20 @@ const getData = async () => {
 };
 
 getData();
+
+
+$nextButton.addEventListener("click", event => {
+    if(offset >= limit - offsetIncrease){
+        return
+    }
+    offset += offsetIncrease
+    getData()
+})
+
+$prevButton.addEventListener("click", event => {
+    if (offset === 0){
+        return
+    }
+    offset -= offsetIncrease
+    getData()
+})
