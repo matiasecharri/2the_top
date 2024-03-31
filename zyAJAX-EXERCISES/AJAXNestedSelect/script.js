6d83b1f4dab2a5b43bd1f81097ef4e37586bf6cb
matiasecharri <matiasecharri@hotmail.com>
2024-03-30 22:06:59 -0300

    feat: provinces

--- zyAJAX-EXERCISES/AJAXNestedSelect/script.js
+++ zyAJAX-EXERCISES/AJAXNestedSelect/script.js
@@ -1,11 +1,20 @@
-fetch("https://apis.datos.gob.ar/georef/api/provincias")
-  .then(res => (res.ok ? res.json() : Promise.reject()))
-  .then(json => {
-    const data = json;
-    console.log(data);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+const getProvinces = async () => {
+  try {
+    const response = await fetch(
+      "https://apis.datos.gob.ar/georef/api/provincias"
+    );
+
+    if (!response.ok) {
+      throw new Error(`ERROR: ${response.status} trying to fetch provinces.`);
+    }
+
+    const data = await response.json();
+    const provinces = data.provincias;
+    console.log(provinces);
+  } catch (error) {
+    console.warn(error);
+  }
+};
+getProvinces();
 
 // https://apis.datos.gob.ar/georef/api/municipios?provincia=22&campos=id,nombre&max=100
