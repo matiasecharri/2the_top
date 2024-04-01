cd8bd74e9c3bdbf8c634e3b66d39f1d5d64fc0d4
matiasecharri <matiasecharri@hotmail.com>
2024-04-01 19:35:31 -0300

    feat: added ?_embed to avoid 4 multiple fetchs

--- zyAJAX-EXERCISES/AJAXWordPressAPI/script.js
+++ zyAJAX-EXERCISES/AJAXWordPressAPI/script.js
@@ -2,8 +2,8 @@ const NAME = `coolado`,
   DOMAIN = `https://${NAME}.com`,
   SITE = `${DOMAIN}/wp-json`,
   API_WP = `${SITE}/wp/v2`,
-  POSTS = `${API_WP}/posts`,
-  PAGES = `${API_WP}/pages`,
+  POSTS = `${API_WP}/posts?_embed`,
+  PAGES = `${API_WP}/pages?_embed`,
   CATEGORIES = `${API_WP}/categories`;
 
 const getData = async () => {
