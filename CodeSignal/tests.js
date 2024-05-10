b86101fdf6e4e5b23c4e03c26d2ee950ca1c6e53
matiasecharri <matiasecharri@hotmail.com>
2024-05-09 22:15:31 -0300

    build: smoke animation

--- CodeSignal/tests.js
+++ CodeSignal/tests.js
@@ -1,7 +1,23 @@
 /* The Journey Begins */
-const solution = (param1, param2) => param1 + param2
+const solution = (param1, param2) => param1 + param2;
 
-const solution2 = (year) => Math.ceil(year / 100)
+const solution2 = year => Math.ceil(year / 100);
 
-const solution3 = (string) => string.toLowerCase() === [...string].reverse().join("").toLowerCase() ? true : false
+const solution3 = string =>
+    string.toLowerCase() === [...string].reverse().join("").toLowerCase()
+        ? true
+        : false;
 
+/* Edge of the Ocean */
+
+const $mouseFollower = document.querySelector(".mouse-follower");
+
+document.addEventListener("mousemove", event => {
+    $mouseFollower.style.setProperty(
+        "transform",
+        `translate(${event.clientX}px, ${event.clientY}px)`
+    );
+    event.target.matches("a")
+        ? (($mouseFollower.style.fontSize = "2rem"), (($mouseFollower.textContent = "🔥")))
+        : (($mouseFollower.textContent === "🔥") ? (($mouseFollower.style.fontSize = "1rem"), (($mouseFollower.textContent = "⚫"))) : null)
+});
